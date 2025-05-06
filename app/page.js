f95d2ca86dfc07ff7e9b6bbfc6529b87dc1dc490
poornima <botsavenkatasai@gmail.com>
2025-05-06 15:28:42 +0530

    Adding pages

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main className="text-center p-10">
+        <h1 className="text-3xl font-bold">BOTSA POORNIMA</h1>
+        <p className="mt-4">Welcome to my portfolio built with Next.js and Tailwind CSS!</p>
+      </main>
+    );
+  }
+  
